c93c9425359c7c2fe0a0c9dcf13a82745068bf8f
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-09 19:47:13 +0300

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -4,6 +4,10 @@ public class LoginTest {
     @Test
    public void verifyUserCanLogin(){
         System.out.println("verify login successful");
+   }
 
+   @Test
+    public void verifyUserCannotLoginWithInvalidCredentials(){
+        
    }
 }
