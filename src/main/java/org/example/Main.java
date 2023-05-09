576c0c8a75602b4e808cae77238144ddc23bca46
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-09 19:04:10 +0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,11 @@ package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
     }
 }
