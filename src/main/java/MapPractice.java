84c221183749aab408dbd3c0b2b4b0d84f2da0f6
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-09 19:06:42 +0300

    Update MapPractice.java

--- src/main/java/MapPractice.java
+++ src/main/java/MapPractice.java
@@ -1,5 +1,8 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapPractice {
     public static void main(String[] args) {
-
+        Map<String, Integer> salary = new HashMap<>();
     }
 }
