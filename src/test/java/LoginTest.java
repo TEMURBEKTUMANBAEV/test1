ed65b1b47ed12808f4c8093743d02be6d0cf1ad7
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-09 19:52:33 +0300

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,21 +3,21 @@ import org.junit.Test;
 public class LoginTest {
     @Test
    public void verifyUserCanLogin(){
-        System.out.println("Opening browser...");
+
         System.out.println("verify login successful");
         System.out.println("Close browser...");
    }
 
    @Test
     public void verifyUserCannotLoginWithInvalidCredentials(){
-       System.out.println("Opening browser...");
+
        System.out.println("verify login unsuccessful for invalid credentials");
        System.out.println("Close browser...");
    }
 
    @Test
     public void verifyUserCanResentPassword(){
-        System.out.println("Opening browser...");
+        
        System.out.println("verify user can resent password");
        System.out.println("Close browser...");
    }
