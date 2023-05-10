47dd5dc84b64b4819bd7a0b2ff2d740c5b9ebab3
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-10 16:37:25 +0300

    Update TestRunner.java

--- src/test/java/com/automation/runner/TestRunner.java
+++ src/test/java/com/automation/runner/TestRunner.java
@@ -1,9 +1,10 @@
 package com.automation.runner;
 
+import com.automation.test.LoginTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-
+@Suite.SuiteClasses({LoginTest.class, SecurityException.class})
 public class TestRunner {
 }
