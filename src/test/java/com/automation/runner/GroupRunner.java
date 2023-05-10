a1a1cd4a303970b9db9139e302d8a9b07815ff03
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-10 16:46:35 +0300

    Update GroupRunner.java

--- src/test/java/com/automation/runner/GroupRunner.java
+++ src/test/java/com/automation/runner/GroupRunner.java
@@ -1,5 +1,6 @@
 package com.automation.runner;
 
+import com.automation.catagories.SmokeTest;
 import com.automation.test.CartPageTest;
 import com.automation.test.LoginTest;
 import com.automation.test.SearchFunctionalityTest;
@@ -9,5 +10,6 @@ import org.junit.runners.Suite;
 
 @RunWith(Categories.class)
 @Suite.SuiteClasses({CartPageTest.class, LoginTest.class, SearchFunctionalityTest.class})
+@Categories.IncludeCategory(SmokeTest.class)
 public class GroupRunner {
 }
