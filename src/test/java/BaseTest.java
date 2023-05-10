d5f8d9c0cda10d09dcb95ac2a68de331eccedecc
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-10 16:16:48 +0300

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,3 +1,17 @@
+import org.junit.After;
+import org.junit.Before;
+
 public class BaseTest {
+
+    @Before
+    public void setUP(){
+        System.out.println("Opening browser...");
+
+    }
+    @After
+    public  void cleanUp(){
+        System.out.println("Close browser...");
+    }
+
 
 }
