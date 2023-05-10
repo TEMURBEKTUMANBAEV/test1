237d4b39317eab4f84f96d62e9f96ae87f1ddccb
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-10 16:43:29 +0300

    Update CartPageTest.java

--- src/test/java/com/automation/test/CartPageTest.java
+++ src/test/java/com/automation/test/CartPageTest.java
@@ -1,9 +1,13 @@
 package com.automation.test;
 
+import com.automation.catagories.SmokeTest;
 import com.automation.test.BaseTest;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 public class CartPageTest extends BaseTest {
+
+    @Category(SmokeTest.class)
     @Test
     public void verifyUserCanAddProductToCart(){
         System.out.println("verify user can add item");
