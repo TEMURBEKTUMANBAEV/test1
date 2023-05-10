f5b076567aecc740081e22e7ce8cabb1e8e9d9c2
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-10 16:26:00 +0300

    Update SearchFunctionalityTest.java

--- src/test/java/SearchFunctionalityTest.java
+++ src/test/java/SearchFunctionalityTest.java
@@ -3,15 +3,15 @@ import org.junit.Test;
 public class SearchFunctionalityTest extends BaseTest {
 @Test
     public void verifySearchResultsAreDisplayed(){
-
+    System.out.println();
 }
 @Test
     public void verifyMessageWhenThereAreNoResults(){
-
+    System.out.println();
 }
 
 @Test
     public void verifySearchResultHasSearchKeyword(){
-    
+    System.out.println();
 }
 }
