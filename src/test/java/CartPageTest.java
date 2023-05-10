3dcfa025b497fa720a689becee3535752a3a45ca
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-10 16:10:51 +0300

    Update CartPageTest.java

--- src/test/java/CartPageTest.java
+++ src/test/java/CartPageTest.java
@@ -3,16 +3,16 @@ import org.junit.Test;
 public class CartPageTest {
     @Test
     public void verifyUserCanAddProductToCart(){
-
+        System.out.println();
     }
 
     @Test
     public void verifyUserCanRemoveItemFromCart(){
-
+        System.out.println();
     }
 
     @Test
     public void verifyUserCanChangeQuantityOfItem(){
-        
+        System.out.println();
     }
 }
