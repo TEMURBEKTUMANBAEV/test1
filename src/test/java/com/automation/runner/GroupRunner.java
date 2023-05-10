16774e450046ad76698daf021b898933038c097b
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-10 16:39:52 +0300

    test

---
+++ src/test/java/com/automation/runner/GroupRunner.java
@@ -0,0 +1,8 @@
+package com.automation.runner;
+
+import org.junit.experimental.categories.Categories;
+import org.junit.runner.RunWith;
+
+@RunWith(Categories.class)
+public class GroupRunner {
+}
--- src/test/java/com/automation/runner/TestRunner.java
+++ src/test/java/com/automation/runner/TestRunner.java
@@ -5,6 +5,5 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({LoginTest.class, SecurityException.class})
 public class TestRunner {
 }
