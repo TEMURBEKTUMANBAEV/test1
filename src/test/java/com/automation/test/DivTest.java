23a5ee0e709187484b0e5de252cfe40dd347ec35
Temurbek <116052673+TEMURBEKTUMANBAEV@users.noreply.github.com>
2023-05-13 16:21:22 +0300

    Update DivTest.java

--- src/test/java/com/automation/test/DivTest.java
+++ src/test/java/com/automation/test/DivTest.java
@@ -3,7 +3,7 @@ package com.automation.test;
 //1. a = 10, b = 5, expected = 2
 //2. a =4 , b = 0, expected = exception
 //3. a = 10, b = 10, expected = 1
-
+//4. a = -10, b = 5, expected = -2
 
 public class DivTest {
 
